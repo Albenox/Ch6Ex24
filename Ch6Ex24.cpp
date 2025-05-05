22aa5ceb0090994c7ec9d75584e087dd51a96323
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-05-05 16:19:18 -0400

    V1

--- Ch6Ex24.cpp
+++ Ch6Ex24.cpp
@@ -15,48 +15,51 @@ void inputChecker(int& choice);
 void menuDisplay();
 
 int main() {
-    //Function that displays menu options
-    menuDisplay();
-    
-    //Declares choice variables and allows input
-    int choice;
-    cin >> choice;
-    inputChecker(choice);
+    while (true) {
+        //Function that displays menu options
+        menuDisplay();
 
-    //Declares the what the selection would need to be in order to win, in the following correlation:
-    // 1 Rock
-    // 2 Paper
-    // 3 Scissors
-    const int rules[4] = { 0,3,1,2 };
-    const string names[4] = { "", "Rock", "Paper", "Scissors"};
-    int i = 1;
+        //Declares choice variables and allows input
+        int choice;
+        cin >> choice;
+        inputChecker(choice);
 
-    while (i == 1) {
-        //Generates random number
-        srand(time(0));
-        int randChoice = rand() % 3 + 1;
-        cout << endl << names[choice] << " selected" << endl;
-        cout << "The computer chose " << names[randChoice] << endl << endl;
+        //Declares the what the selection would need to be in order to win, in the following correlation:
+        // 1 Rock
+        // 2 Paper
+        // 3 Scissors
+        const int rules[4] = { 0,3,1,2 };
+        const string names[4] = { "", "Rock", "Paper", "Scissors" };
+        int i = 1;
 
-        //Tie case
-        if (choice == randChoice) {
-            cout << "You and the computer both chose " << names[choice] << endl;
-            menuDisplay();
-            cin >> choice;
-            i = 1;
-            cin.clear();
-        }
-        //Win case
-        else if (rules[choice] == randChoice) {
-            cout << "You win, " << names[choice] << " beats " << names[randChoice] << endl;
-            i = 0;
-            cin.clear();
-        }
-        //Loss case
-        else {
-            cout << "You lose, " << names[randChoice] << " beats " << names[choice] << endl;
-            i = 0;
-            cin.clear();
+        //It just does
+        while (i == 1) {
+            //Generates random number
+            srand(time(0));
+            int randChoice = rand() % 3 + 1;
+            cout << endl << names[choice] << " selected" << endl;
+            cout << "The computer chose " << names[randChoice] << endl << endl;
+
+            //Tie case
+            if (choice == randChoice) {
+                cout << "You and the computer both chose " << names[choice] << endl;
+                menuDisplay();
+                cin >> choice;
+                i = 1;
+                cin.clear();
+            }
+            //Win case
+            else if (rules[choice] == randChoice) {
+                cout << "You win, " << names[choice] << " beats " << names[randChoice] << endl;
+                i = 0;
+                cin.clear();
+            }
+            //Loss case
+            else {
+                cout << "You lose, " << names[randChoice] << " beats " << names[choice] << endl;
+                i = 0;
+                cin.clear();
+            }
         }
     }
 }
